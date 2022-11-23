8113a16e6b379817c530ccb5d90e83649c0dbd03
oksana.kovalyk <oksanaztk@gmail.com>
2022-11-23 23:09:50 +0100

    changed apikey

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "23bd3afe644bf1fdo0bt62589c58a8f7";
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
   axios.get(apiUrl).then(displayTemperature);
