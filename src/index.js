f513fa9f0ec00a02ee8c8e1e0fbf6e2f2415d7af
oksana.kovalyk <oksanaztk@gmail.com>
2022-11-23 13:35:49 +0100

    clear extra files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-import App from "./app";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
